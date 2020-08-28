d52f094804337d509bf31229e3d54c9d168765a6
bogdan <bshelomanov98@mail.ru>
2020-08-28 18:23:34 +0300

    jest mock callbak

--- src/components/input/input.spec.js
+++ src/components/input/input.spec.js
@@ -4,12 +4,25 @@ import { shallow } from "enzyme";
 
 /**
  * В компоненте нет логики поэтому достаточно протестировать его рендер и деволтные пропсы
+ *
+ * обработчики компонента не влияют на его состояние, но это можно протестировать
+ * с помощью mock функцию у jest
  * */
 
 describe("Input component", () => {
   it("should rener Input component", () => {
     const component = shallow(<Input />);
     expect(component).toMatchSnapshot();
+  });
+
+  it("should call onChange", () => {
+    const mockCallBack = jest.fn();
+    expect(mockCallBack.mock.calls.length).toBe(0);
+
+    const component = shallow(<Input onChange={mockCallBack} />);
+    component.find(".input").simulate("change");
+
+    expect(mockCallBack.mock.calls.length).toBe(1);
   });
 
   describe("defaultProps", () => {
